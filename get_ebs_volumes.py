8bfb584338e134c598ae3b80c6c9e56ae51d6a28
Matt Wise <matt@nextdoor.com>
2015-12-31 15:46:31 -0800

    bugfixes

--- get_ebs_volumes.py
+++ get_ebs_volumes.py
@@ -116,9 +116,9 @@ def get_ebs_volumes(ebs_vol_list, volcount, volsize, volume_type='standard'):
             time.sleep(5)
             vol.attach(instanceid, dest.replace('xvd', 'sd'))
 
-    while not hasattr(vol.attach_data, 'instance_id'):
-        time.sleep(1)
-        vol.update()
+        while not hasattr(vol.attach_data, 'instance_id'):
+            time.sleep(1)
+            vol.update()
         while not str(vol.attach_data.instance_id) == instanceid or not os.path.exists(dest) == True:
             print("INFO: (%s) Volume attaching..." % (vol.id))
             time.sleep(1)
@@ -139,92 +139,6 @@ def get_ebs_volumes(ebs_vol_list, volcount, volsize, volume_type='standard'):
 
     # All done. Return whatever volumes were created and attached.
     return attached_ebs_vol_list
-
-
-def find_ebs_volumes(ebs_vol_list, ebs_volid_list):
-    """Search Amazon for existing EBS Volume ids in our zone. If they exist,
-    then mount them and return them. If they don't exist, we error out."""
-
-    # Some local instance ID info..
-    zone = commands.getoutput("wget -q -O - http://169.254.169.254/latest/meta-data/placement/availability-zone")
-    region = zone[:-1]
-    instanceid = commands.getoutput("wget -q -O - http://169.254.169.254/latest/meta-data/instance-id")
-    available_ebs_vol_list = []
-    attached_ebs_vol_list = []
-
-    # Make sure that the device list we got is good. If a device exists already,
-    # remove it from the potential 'device targets'
-    for potential_volume in ebs_vol_list:
-        if not os.path.exists(potential_volume):
-            print("INFO: (%s) is available as a disk target." % (potential_volume))
-            available_ebs_vol_list.append(potential_volume)
-
-    # Reverse our available_ebs_vol_list so that we can 'pop' from the beginning
-    available_ebs_vol_list.reverse()
-
-    # Make sure we have enough target devices available
-    if available_ebs_vol_list <= ebs_volid_list.__len__():
-        print("ERROR: Do not have enough local volume targets available to attach the drives. Erroring out.")
-        return False
-
-    # Open our EC2 connection
-    print("INFO: Connecting to Amazon...")
-    ec2 = boto.ec2.connect_to_region(region)
-
-    # For each volume..
-    for ebs_volid in ebs_volid_list:
-        print "INFO: (%s) Searching for EBS volume..." % (ebs_volid)
-        vols = ec2.get_all_volumes(volume_ids=ebs_volid)
-        vol = vols[0]
-
-        # Check if the volume is attached. If it is, bail!
-        if not str(vol.attach_data.status) == "None" \
-            and not str(vol.attach_data.instance_id) == instanceid:
-            print "ERROR: (%s) is attached to instance ID %s already. Exiting!" % (vol.id, vol.attach_data.instance_id)
-            return False
-        # If its attached, but to our host already then figure out
-        # what device its attached to.
-        elif not str(vol.attach_data.status) == "None" \
-            and str(vol.attach_data.instance_id) == instanceid:
-            print "WARNING: (%s) is already attached our instance ID at %s. Using that..." % (vol.id, vol.attach_data.device)
-            dest = vol.attach_data.device
-        else:
-            ## Grab a volume off of our stack of available vols..
-            dest = available_ebs_vol_list.pop()
-            # Attach the volume and wait for it to fully attach
-            print "INFO: (%s) Attaching EBS volume to our instance ID (%s) to %s" % (vol.id, instanceid, dest)
-            vol.attach(instanceid, dest.replace('xvd','sd'))
-            while not hasattr(vol.attach_data, 'instance_id'):
-                time.sleep(1)
-                vol.update()
-            while not str(vol.attach_data.instance_id) == instanceid \
-                or not os.path.exists(dest) == True:
-                print "INFO: (%s) Volume attaching..." % (vol.id)
-                time.sleep(1)
-                vol.update()
-            # Sleep a few more seconds just to make sure the OS has seen the volume
-            time.sleep(1)
-
-        # Check whether we are using /dev/xvd volumes or /dev/sd volumes. Amazon always returns a volume mount
-        # point as '/dev/sdXXX' when sometimes its actually '/dev/xvdXXX'.
-        if os.path.exists("/dev/xvda1"):
-            dest = dest.replace('sd','xvd')
-            print "INFO: (%s) Converting volume mount point to %s" % (vol.id, dest)
-
-        # Add the volume to our list of volumes that were created
-        attached_ebs_vol_list.append(dest)
-        print "INFO: (%s) Volume attached!" % (vol.id)
- 
-        # Now, tag the volumes and move on
-        tags = {}
-        tags["Name"] = "%s-%s" % (socket.gethostname(), dest)
-        print "INFO: (%s) Taggin EBS volume with these tags: %s" % (vol.id, tags)
-        ec2.create_tags(str(vol.id), tags)
-
-    # All done. Return whatever volumes were created and attached.
-    return attached_ebs_vol_list
-    
-#### END FUNCTIONS ####
 
 # Sanity check.. depending on our kernel, we use different volumes for EBS. Pick those here
 if os.path.exists("/dev/sda1"):
